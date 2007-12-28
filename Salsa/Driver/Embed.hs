a257ec539610829199b5a65fd77086824c58a11a
Andrew Appleyard <aja@cse.unsw.edu.au>
2007-12-28 14:05:46 +1100

    Code generator for embedded .NET assembly
    darcs-hash:20071228030546-8c545-95b6c28d57127e2ba0a0517832b29f7c0b462127.gz

---
+++ Salsa/Driver/Embed.hs
@@ -0,0 +1,29 @@
+-----------------------------------------------------------------------------
+-- |
+-- Program     : Embed
+-- Copyright   : (c) 2007 Andrew Appleyard
+-- Licence     : BSD-style (see LICENSE.txt)
+-- 
+-- Generates a Haskell module that contains the binary data of the Salsa
+-- driver assembly (Salsa.dll) as an (unboxed) string literal.  This is
+-- used to embed the driver assembly in every program that uses Salsa.
+--
+-----------------------------------------------------------------------------
+module Main where
+
+import System.Environment
+import qualified Data.ByteString as S
+import Text.Printf
+
+main :: IO ()
+main = do
+    putStrLn "module Salsa.Driver (driverData) where"
+    putStrLn "import qualified Data.ByteString.Char8 as B"
+    putStrLn "{-# NOINLINE driverData #-}"
+    putStr "driverData = B.pack \""
+    [inputFile] <- getArgs
+    s <- S.readFile inputFile --"Salsa.dll"
+    mapM_ (\x -> printf "\\x%02x" x) (S.unpack s)
+    putStrLn "\""
+
+-- vim:set ts=4 sw=4 expandtab:
