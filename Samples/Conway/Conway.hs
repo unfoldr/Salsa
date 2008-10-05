ca01ca9b77dbf59906f79a43b0c541bbc27f9834
Andrew Appleyard <unfoldr@gmail.com>
2008-10-06 01:35:42 +1100

    Updated the conway sample to use the new syntax
    darcs-hash:20081005143542-b17f6-5690fa8171e6ce6058a92f3fc9b67e650f174b2d.gz

--- Samples/Conway/Conway.hs
+++ Samples/Conway/Conway.hs
@@ -10,11 +10,11 @@ import Data.Maybe
 
 loadXaml :: Coercible (Obj Object_) a => String -> IO a
 loadXaml xamlPath = do
-    uri <- new Uri_ (xamlPath, UriKind_ # _Relative_)
-    streamInfo <- Application_ # _GetRemoteStream (uri)
-    xamlReader <- new XamlReader_ ()
-    stream     <- get streamInfo Stream_
-    get streamInfo Stream_ >>= \s -> xamlReader # _LoadAsync s >>= cast
+    uri <- new _Uri (xamlPath, _UriKind # _relative_)
+    streamInfo <- _Application # _getRemoteStream (uri)
+    xamlReader <- new _XamlReader ()
+    stream     <- get streamInfo _Stream
+    get streamInfo _Stream >>= \s -> xamlReader # _loadAsync s >>= cast
 
 -- | @'neighbors' states cell@ is the number of alive cells adjacent to @cell@ in @states@.
 neighbors :: Array (Int,Int) Bool -> (Int,Int) -> Int
@@ -29,7 +29,7 @@ neighbors states (row,col) =
 
 step :: Array (Int,Int) (Obj ToggleButton_) -> IO ()
 step buttons = do
-    buttonStates <- mapM (\b -> get b IsChecked_) (elems buttons)
+    buttonStates <- mapM (\b -> get b _IsChecked) (elems buttons)
     let states = listArray (bounds buttons) (map fromJust buttonStates)
     sequence_ $ do
         cell <- indices states
@@ -39,41 +39,42 @@ step buttons = do
                         otherwise -> Just False -- dies
         case state' of
             Nothing   -> mzero
-            otherwise -> return $ set (buttons ! cell) [ IsChecked_ :== state' ]
+            otherwise -> return $ set (buttons ! cell) [ _IsChecked :== state' ]
 
 main :: IO ()
 main = withCLR $ do
     win :: Obj Window_ <- loadXaml "/Conway.xaml"
 
     -- Build an array of toggle buttons in 'grid'
-    grid :: Obj UniformGrid_ <- win # _FindName ("grid") >>= cast
+    grid :: Obj UniformGrid_ <- win # _findName ("grid") >>= cast
     let (rows,cols) = (12,20)
-    set grid [Columns_ :== fromIntegral cols]
+    set grid [_Columns :== fromIntegral cols]
     buttonList <- sequence $ replicate (rows * cols) $ do
-        b <- new ToggleButton_ ()
-        get grid Children_ >>=# _Add (b)
+        b <- new _ToggleButton ()
+        get grid _Children >>=# _add (b)
         return b
     let buttons = listArray ((0,0),(rows-1,cols-1)) buttonList
 
-    timer <- new DispatcherTimer_ ()
-    set timer [Tick_     :+> delegate EventHandler_ (\_ _ -> step buttons),
-               Interval_ :=> TimeSpan_ # _FromSeconds (0.1::Double)]
+    timer <- new _DispatcherTimer ()
+    set timer [_Tick     :+> delegate _EventHandler (\_ _ -> step buttons),
+               _Interval :=> _TimeSpan # _fromSeconds (0.1::Double)]
 
-    runButton :: Obj ToggleButton_ <- win # _FindName ("runButton") >>= cast
-    set runButton [Click_ :+> delegate RoutedEventHandler_ 
-        (\_ _ -> do Just run <- get runButton IsChecked_
-                    if run then timer # _Start ()
-                           else timer # _Stop ())]
+    runButton :: Obj ToggleButton_ <- win # _findName ("runButton") >>= cast
+    set runButton [_Click :+> delegate _RoutedEventHandler
+        (\_ _ -> do Just run <- get runButton _IsChecked
+                    if run then timer # _start ()
+                           else timer # _stop ())]
 
-    clearButton :: Obj Button_ <- win # _FindName ("clearButton") >>= cast
-    set clearButton [Click_ :+> delegate RoutedEventHandler_
-        (\_ _ -> mapM_ (flip set [ IsChecked_ :== Just False ]) (elems buttons))]
+    clearButton :: Obj Button_ <- win # _findName ("clearButton") >>= cast
+    set clearButton [_Click :+> delegate _RoutedEventHandler
+        (\_ _ -> mapM_ (flip set [ _IsChecked :== Just False ]) (elems buttons))]
         
-    exitButton :: Obj Button_ <- win # _FindName ("exitButton") >>= cast
-    set exitButton [Click_ :+> delegate RoutedEventHandler_ (\_ _ -> win # _Close_)]
+    exitButton :: Obj Button_ <- win # _findName ("exitButton") >>= cast
+    set exitButton [_Click :+> delegate _RoutedEventHandler (\_ _ -> win # _close_)]
     
-    app <- new Application_ ()
-    app # _Run (win)
+    app <- new _Application ()
+    app # _run (win)
+
     return ()
 
 -- vim:set sw=4 ts=4 expandtab:
